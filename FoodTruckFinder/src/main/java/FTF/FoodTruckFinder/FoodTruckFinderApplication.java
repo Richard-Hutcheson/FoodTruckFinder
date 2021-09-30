1b1b51f72bcadcf8f325b54b01791ec3329a03d1
Richard-Hutcheson <richardhutch80@gmail.com>
2021-09-29 23:03:47 -0500

    added spring

---
+++ FoodTruckFinder/src/main/java/FTF/FoodTruckFinder/FoodTruckFinderApplication.java
@@ -0,0 +1,13 @@
+package FTF.FoodTruckFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckFinderApplication.class, args);
+	}
+
+}
---
+++ FoodTruckFinder/src/test/java/FTF/FoodTruckFinder/FoodTruckFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package FTF.FoodTruckFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
