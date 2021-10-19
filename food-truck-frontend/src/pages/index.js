16365b60d4d6a5eaf4412b14613b01e62e19f277
Richard-Hutcheson <richardhutch80@gmail.com>
2021-10-18 22:44:27 -0500

    added navigation and css to login screen

--- food-truck-frontend/src/pages/index.js
+++ food-truck-frontend/src/pages/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 
+
 require('dotenv').config();
 
 function HomePage() {
     return (
-        <div class = "header">
-            <h1>Food Truck Finder</h1>
-            <h2>LOGIN</h2>
-            <Link href="/dashboard">
-                <a>login</a>
-            </Link>
+        <body class = 'login-body'>
+            
+            <div class = "login-header">
+                <h1 id = 'ftf-header'>FOOD TRUCK FINDER</h1>
+            </div>
 
-        </div>
+
+            <div class = "login-wrapper">
+                <div class = "username-field">
+                    <label for="username" id = "username-label">username:</label>
+                    <input type="text" id="username" name="username"></input>
+                </div>
+                <div class = "password-field">
+                    <label for="pass" id = "password-label">password:</label>
+                    <input type="password" id="pass" name="password" minlength="8" required></input>
+                </div>
+            </div>
+
+            <div class = 'login-btn-class'>
+                <Link href="/dashboard">
+                    <a id = 'login-btn-id'>login</a>
+                </Link>
+            </div>
+            <footer>
+                <p>Ethan Robinson, Austin Blanchard, Richard Hutcheson, Noah Lambaria</p>
+            </footer> 
+        </body>
+
     )
 }
 
