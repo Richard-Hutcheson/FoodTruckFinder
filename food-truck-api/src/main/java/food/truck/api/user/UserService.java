39d993530320229d29484a24494cc53a3bce89de
mr-noah <noah134567@gmail.com>
2021-10-20 00:59:52 -0500

    fixed some issues regarding hibernate

--- food-truck-api/src/main/java/food/truck/api/user/UserService.java
+++ food-truck-api/src/main/java/food/truck/api/user/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
+    @Autowired
     private UserRepository userRepository;
 
     public Optional<User> findUser(Long userId) {
