e0ca839d84c5a0b16ad9ba57558b286b617418a8
Nitsua365 <austinblanchard@hotmail.com>
2021-10-26 20:47:25 -0500

    cleaned up

--- food-truck-api/src/main/java/ftf/App.java
+++ food-truck-api/src/main/java/ftf/App.java
@@ -1,5 +1,6 @@
 package ftf;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
