2c6870f80181d4ef7ef99af04f974c4a9c09f611
Richard-Hutcheson <richardhutch80@gmail.com>
2021-11-10 20:36:06 -0600

    update

--- food-truck-api/src/main/java/ftf/Controller/RecommendationsController.java
+++ food-truck-api/src/main/java/ftf/Controller/RecommendationsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class RecommendationsController {
 
