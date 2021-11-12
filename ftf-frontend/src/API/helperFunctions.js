18eefb6df68ec564fb99aa22134e0bcd21605d3b
Retro5050 <Ethan_Robinson2@baylor.edu>
2021-11-12 15:06:16 -0600

    Creating new Frontend folder

--- ftf-frontend/src/API/helperFunctions.js
+++
@@ -1,5 +0,0 @@
-export function getFoodTypes(){
-    let typeList = ["AMERICAN", "CHINESE", "ITALIAN", "MEXICAN", "GREEK", "KOREAN", "JAPANESE",
-    "VIETNAMESE", "THAI", "INDIAN", "FRENCH", "GERMAN", 'DESSERT', 'DRINK'];
-    return typeList;
-}
--- ftf-frontend/src/index.js
+++ ftf-frontend/src/index.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginScreen from './pages/loginScreen.js';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import UserDashboard from './pages/userDashboard.js'
-import CreateAccount from './pages/createAccount.js'
-import PageNotFound from './pages/pageNotFound.js'
-import UserManageAccount from './pages/userManageAccnt'
-import TruckOwnerDashboard from './pages/truckOwnerDashboard.js';
-import ManageFoodTrucks from './pages/manageFoodTrucks.js';
-import SearchResult from './pages/searchResult.js';
-import EditTruck from './pages/editFoodTruck.js';
-import AddTruck from './pages/addFoodTruck.js';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    
-    <Router>
-        <Switch>
-            <Route path="/" exact component={LoginScreen}/>
-            <Route path="/UserDashboard" exact component={UserDashboard}/>
-            <Route path = "/CreateAccount" exact component = {CreateAccount}/>
-            <Route path = "/ManageAccount" exact component = {UserManageAccount}/>
-            <Route path = "/TruckOwnerDashboard" exact component={TruckOwnerDashboard}/>
-            <Route path = "/ManageFoodTrucks" exact component={ManageFoodTrucks}/>
-            <Route path = "/SearchResult" exact component = {SearchResult}/>
-            <Route path = "/EditTruck" exact component = {EditTruck}/>
-            <Route path = "/AddTruck" exact component = {AddTruck}/>
-            <Route component={PageNotFound}/>
-        </Switch>
-    </Router>,
-     document.getElementById("root"));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
