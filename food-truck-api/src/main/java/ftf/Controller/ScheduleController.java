8e8704303795999171c6f263178f1627475c0e72
mr-noah <noah134567@gmail.com>
2021-11-18 11:34:11 -0600

    added schedule functionality for backend:

--- food-truck-api/src/main/java/ftf/Controller/ScheduleController.java
+++ food-truck-api/src/main/java/ftf/Controller/ScheduleController.java
@@ -1,4 +1,36 @@
 package ftf.Controller;
 
+import ftf.Repository.ScheduleRepository;
+import ftf.Service.ScheduleService;
+import ftf.classes.Schedule;
+import ftf.classes.User;
+import ftf.exceptions.UserNotFoundException;
+import ftf.exceptions.UsernameTakenException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class ScheduleController {
+    @Autowired
+    ScheduleService scheduleService;
+
+    @PostMapping("/save/schedule")
+    public Schedule saveSchedule(@RequestBody Schedule schedule) {
+        return scheduleService.save(schedule);
+    }
+
+    @PatchMapping("/update/{truckName}")
+    public Schedule updateSchedule(@PathVariable String truckName, @RequestBody Schedule updatedSchedule) {
+        return scheduleService.updateSchedule(updatedSchedule,truckName);
+    }
+
+    @DeleteMapping("/delete/schedule/{ftName}")
+    public void deleteSchedule( @PathVariable String ftName) {
+        scheduleService.deleteSchedule(ftName);
+    }
 }
---
+++ food-truck-api/src/main/java/ftf/Service/ScheduleService.java
@@ -0,0 +1,72 @@
+package ftf.Service;
+
+import ftf.Repository.FoodTruckRepository;
+import ftf.Repository.ScheduleRepository;
+import ftf.classes.FoodTruck;
+import ftf.classes.Schedule;
+import ftf.classes.Subscription;
+import ftf.classes.User;
+import ftf.exceptions.FoodTruckNotFoundException;
+import ftf.exceptions.SubscriptionDoesntExist;
+import ftf.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ScheduleService {
+    @Autowired
+    FoodTruckRepository foodTruckRepository;
+    @Autowired
+    ScheduleRepository scheduleRepository;
+
+
+    public Schedule save(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+    }
+
+    public Schedule updateSchedule(Schedule schedule,String truckName) {
+        Optional<FoodTruck> foodTruck = foodTruckRepository.findFoodTruckByTruckName(truckName);
+        if(!foodTruck.isPresent()){
+            throw new FoodTruckNotFoundException("Food Truck not found");
+        }
+
+        Optional<Schedule> foundSchedule = scheduleRepository.findScheduleByTruck(foodTruck.get());
+
+        if (!foundSchedule.isPresent()) {
+            throw new RuntimeException("Schedule not found");
+        }
+
+
+        Schedule updatedSchedule = foundSchedule.get();
+
+
+        updatedSchedule.setTruck(schedule.getTruck());
+        updatedSchedule.setMonday(schedule.getMonday());
+        updatedSchedule.setTuesday(schedule.getTuesday());
+        updatedSchedule.setWednesday(schedule.getWednesday());
+        updatedSchedule.setThursday(schedule.getThursday());
+        updatedSchedule.setFriday(schedule.getFriday());
+        updatedSchedule.setSaturday(schedule.getSaturday());
+        updatedSchedule.setSunday(schedule.getSunday());
+
+        scheduleRepository.save(updatedSchedule);
+
+        return updatedSchedule;
+    }
+
+    public void deleteSchedule(String ftName) {
+
+        Optional<FoodTruck> foodTruck = foodTruckRepository.findFoodTruckByTruckName(ftName);
+        if(!foodTruck.isPresent()) {
+            throw new FoodTruckNotFoundException("Food truck not found");
+        }
+        Optional<Schedule> scheduleToDelete = scheduleRepository.findScheduleByTruck(foodTruck.get());
+        if(!scheduleToDelete.isPresent()){
+            throw new RuntimeException("Schedule not found");
+        }
+        scheduleRepository.delete(scheduleToDelete.get());
+
+    }
+}
