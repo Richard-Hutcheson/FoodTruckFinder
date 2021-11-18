4868badd349c0ae43f4e3a865afe1277dbd52f88
Nitsua365 <austinblanchard@hotmail.com>
2021-11-17 19:24:20 -0600

    upgraded search

--- food-truck-api/src/main/java/ftf/Service/SearchService.java
+++ food-truck-api/src/main/java/ftf/Service/SearchService.java
@@ -9,10 +9,7 @@ import ftf.exceptions.RouteNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,26 +21,42 @@ public class SearchService {
     @Autowired
     RouteRepository routeRepository;
 
+    private final String[] fillerWords = {"Food", "Truck"};
+
     public List<FoodTruck> searchTrucks(String searchTrucks) {
 
         String[] searchTrucksList = searchTrucks.split(" ");
 
         ArrayList<FoodTruck> foodTrucks = new ArrayList<>();
+        ArrayList<FoodTruck> notFiller = new ArrayList<>();
 
         for (String str : searchTrucksList) {
-            foodTrucks.addAll(foodTruckRepository.findByTruckNameLike(str));
-            foodTrucks.addAll(foodTruckRepository.findByTruckNameContains(str));
-            foodTrucks.addAll(foodTruckRepository.findFoodTrucksByFoodType(str));
-            foodTrucks.addAll(foodTruckRepository.findFoodTrucksByFoodTypeContains(str));
-            foodTrucks.addAll(foodTruckRepository.findFoodTrucksByFoodTypeLike(str));
+
+            if (!Arrays.toString(fillerWords).toLowerCase().contains(str.toLowerCase())) {
+                notFiller.addAll(foodTruckRepository.findByTruckNameLike(str));
+                notFiller.addAll(foodTruckRepository.findByTruckNameContains(str));
+                notFiller.addAll(foodTruckRepository.findFoodTrucksByFoodType(str));
+                notFiller.addAll(foodTruckRepository.findFoodTrucksByFoodTypeContains(str));
+                notFiller.addAll(foodTruckRepository.findFoodTrucksByFoodTypeLike(str));
+            }
+            else {
+                foodTrucks.addAll(foodTruckRepository.findByTruckNameLike(str));
+                foodTrucks.addAll(foodTruckRepository.findByTruckNameContains(str));
+                foodTrucks.addAll(foodTruckRepository.findFoodTrucksByFoodType(str));
+                foodTrucks.addAll(foodTruckRepository.findFoodTrucksByFoodTypeContains(str));
+                foodTrucks.addAll(foodTruckRepository.findFoodTrucksByFoodTypeLike(str));
+            }
         }
 
-        Set<FoodTruck> foodTruckSet = new HashSet<>(foodTrucks);
+        Set<FoodTruck> finalList = new LinkedHashSet<>();
 
-        if (foodTrucks.isEmpty())
+        finalList.addAll(notFiller);
+        finalList.addAll(foodTrucks);
+
+        if (finalList.isEmpty())
             throw new FoodTruckNotFoundException("Food Truck Not Found");
 
-        return new ArrayList<>(foodTruckSet);
+        return new ArrayList<>(finalList);
     }
 
     public List<Route> searchNearByTrucks(String cityPref) {
