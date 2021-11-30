6ab537363a28d75782ca6fde9926bd59b6dac6ab
Nitsua365 <austinblanchard@hotmail.com>
2021-11-30 16:44:40 -0600

    created createFoodTruck test in JUnit

--- food-truck-api/src/test/java/org/example/AppTest.java
+++ food-truck-api/src/test/java/org/example/AppTest.java
@@ -1,20 +1,37 @@
 package org.example;
 
-//import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
-//import org.junit.Test;
+import ftf.Repository.FoodTruckRepository;
+import ftf.Service.FoodTruckService;
+import ftf.classes.FoodTruck;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 /**
  * Unit test for simple App.
  */
+@SpringBootTest(classes = FoodTruckService.class)
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-//    @Test
-//    public void shouldAnswerWithTrue()
-//    {
-//        assertTrue( true );
-//    }
+
+    @MockBean
+    FoodTruckService foodTruckService;
+
+    @MockBean
+    FoodTruckRepository foodTruckRepository;
+
+    @Test
+    public void testCreateFoodTruck() {
+        FoodTruck saveFT = new FoodTruck();
+        saveFT.setTruckName("Junit Test Truck");
+
+        when(foodTruckService.createNewTruck(saveFT)).thenReturn(saveFT);
+        assertSame(saveFT, foodTruckService.createNewTruck(saveFT));
+    }
 }
